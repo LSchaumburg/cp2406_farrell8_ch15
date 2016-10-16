58551d972f594db362f47f0f507fbed79fd9cd98
Luke <luke.schaumburg@my.jcu.edu.au>
2016-10-17 07:09:13 +1000

    Minor bug fixes

--- DebuggingExercises/FixDebugFifteen2.java
+++ DebuggingExercises/FixDebugFifteen2.java
@@ -5,22 +5,22 @@ import java.awt.*;
 
 public class FixDebugFifteen2 extends JFrame
 {
-   JButton nb = new JButton("Left    ");
-   JButton sb = new JButton("Down  ");
-   JButton eb = new JButton("Right ");
-   JButton wb = new JButton("Center  ");
-   JButton cb = new JButton("Up");
+   JButton northb = new JButton("Left    ");
+   JButton southb = new JButton("Down  ");
+   JButton eastb = new JButton("Right ");
+   JButton westb = new JButton("Center  ");
+   JButton centerb = new JButton("Up");
    Container con = null;
 
    public FixDebugFifteen2()
    {
       con = this.getContentPane();
       con.setLayout(new BorderLayout());
-      con.add(nb, BorderLayout.NORTH);
-      con.add(sb, BorderLayout.SOUTH);
-      con.add(eb, BorderLayout.EAST);
-      con.add(wb, BorderLayout.WEST);
-      con.add(cb, BorderLayout.CENTER);
+      con.add(northb, BorderLayout.NORTH);
+      con.add(southb, BorderLayout.SOUTH);
+      con.add(eastb, BorderLayout.EAST);
+      con.add(westb, BorderLayout.WEST);
+      con.add(centerb, BorderLayout.CENTER);
    }
    public static void main(String[] args)
    {
